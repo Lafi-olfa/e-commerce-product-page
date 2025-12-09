a79ac41c263f4638e6c320156e54d66ec129c2a9
olfalafi <olfalafi1998@gmail.com>
2025-12-09 22:47:33 +0100

    Initialize e commerce project with React, Vite, TypeScript and Tailwind Css

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <button className="rounded bg-blue-500 px-4 py-2 text-base text-white">
+        test{" "}
+      </button>
+    </>
+  );
+}
+
+export default App;
